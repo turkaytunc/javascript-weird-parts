6c76c0b9031c9e576710dea7c795b7bd5d5bdea8
Turkay Tunc <turkaytunc00@gmail.com>
2020-08-14 16:48:48 +0300

    update

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
-var a = "hello!";
+function a() {
+  function b() {
+    console.log(myVar); // 2
+  }
+  var myVar = 2;
+  b();
+}
 
-function b() {}
+var myVar = 5;
+a();
+console.log(myVar); // 5
