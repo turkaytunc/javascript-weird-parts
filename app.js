5406ab0e69d342cd5ad77fe91fe3dcf195c141af
Turkay Tunc <turkaytunc00@gmail.com>
2020-08-14 17:16:15 +0300

    update

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
-function b() {
-  console.log(myVar); // 5
-}
+// function b() {
+//   console.log(myVar); // 5
+// }
 
-function a() {
-  var myVar = 2;
-  b();
-}
+// function a() {
+//   var myVar = 2;
+//   b();
+// }
 
-var myVar = 5;
-a();
-console.log(myVar); // 5
+// var myVar = 5;
+// a();
+// console.log(myVar); // 5
+
+// function waitFiveSeconds() {
+// 	console.time('calc');
+// 	console.log('function started!');
+
+// 	let ms = 5000 + new Date().getTime();
+
+// 	while (new Date() < ms) {}
+
+// 	console.log('function finished');
+// 	console.timeEnd('calc');
+// }
+
+// waitFiveSeconds();
