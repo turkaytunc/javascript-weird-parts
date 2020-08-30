caa681a3411202e738fbef33eb5340625ef44a05
Turkay Tunc <turkaytunc00@gmail.com>
2020-08-30 12:25:01 +0300

    added borrowing currying

--- bindCallApply.js
+++ bindCallApply.js
@@ -18,3 +18,29 @@ logPersonName('en');
 
 logName.call(person, 'en', 'es'); //binds this keyword to given object and calls function
 logName.apply(person, ['en', 'es']); //same as call but takes arguments as an array
+
+//IIFE
+(function (lang1, lang2) {
+  console.log('Logged: ' + this.getFullName());
+  console.log('Arguments: ' + lang1 + ' ' + lang2);
+  console.log('-----------');
+}.apply(person, ['es', 'en']));
+
+// function borrowing
+var person2 = {
+  firstname: 'Jane',
+  lastname: 'Doe',
+};
+
+console.log(person.getFullName.apply(person2)); //borrowed person object's getFullName function. Property names must match, in this case person and person2 objects have firstName and lastName
+
+// function currying
+function multiply(a, b) {
+  return a * b;
+}
+
+var multipleByTwo = multiply.bind(this, 2);
+console.log(multipleByTwo(4));
+
+var multipleByThree = multiply.bind(this, 3);
+console.log(multipleByThree(4));
